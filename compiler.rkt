5fc468496978ab1dad27a62acca898ee78fe9cc7
Kannav <kmkannavkmehta@gmail.com>
2023-01-24 22:08:20 +0530

    A1 code + tests

--- compiler.rkt
+++ compiler.rkt
@@ -9,25 +9,89 @@
 (require "utilities.rkt")
 (provide (all-defined-out))
 
+(define (uniquify-exp env)
+  (lambda (e)
+    (match e
+      [(Var x) (Var (dict-ref env x))]
+      [(Int n) (Int n)]
+      [(Let x e body) (let ([env-new (dict-set env x (gensym x))])
+                        (Let (dict-ref env-new x)
+                             ((uniquify-exp env) e)
+                             ((uniquify-exp env-new) body)))]
+      [(Prim op es)
+       (Prim op (for/list ([e es]) ((uniquify-exp env) e)))]
+      [_ (error "Unrecognized expression (uniquify_exp)")])))
+       
 ;; uniquify : R1 -> R1
 (define (uniquify p)
-  (error "TODO: code goes here (uniquify)"))
+  (match p
+    [(Program '() e) (Program '() ((uniquify-exp '()) e))]))
+
+(define (rco-atom e)
+  (match e
+    [(Var x) (values (Var x) '())]
+    [(Int n) (values (Int n) '())]
+    [(Let x rhs body)
+     (define rhs_exp (rco-exp rhs))
+     (define-values (body_atm body_env) (rco-atom body))
+     (values body_atm (append (list (cons x rhs_exp)) body_env))]
+    [(Prim op es)
+     (define-values (es_new env_new) (for/lists (ee env) [(e es)] (rco-atom e)))
+     (define tmp (gensym "tmp"))
+     (values (Var tmp) (append (append* env_new) (list (cons tmp (Prim op es_new)))))]
+    [_ (error "Unrecognized expression (rco-atom)")]))
+
+
+(define (rco-exp e)
+  (define (env-to-let env e)
+    (match env
+      ['() e]
+      [(list (cons x rhs) rest ...) (Let x rhs (env-to-let rest e))]))
+  (match e
+    [(Var x) (Var x)]
+    [(Int n) (Int n)]
+    [(Let x e body) (Let x (rco-exp e) (rco-exp body))]
+    [(Prim 'read '()) (Prim 'read '())]
+    [(Prim op es)
+     (define-values (es_new env_new) (for/lists (ee env) [(e es)] (rco-atom e)))
+     (env-to-let (append* env_new) (Prim op es_new))]
+    [_ (error "Unrecognized expression (rco-exp)")]))
 
 ;; remove-complex-opera* : R1 -> R1
 (define (remove-complex-opera* p)
-  (error "TODO: code goes here (remove-complex-opera*)"))
+  (match p
+    [(Program info e) (Program info (rco-exp e))]))
+
+(define (explicate-tail e)
+  (match e
+    [(Var x) (Return (Var x))]
+    [(Int n) (Return (Int n))]
+    [(Let x rhs body) (explicate-assign rhs x (explicate-tail body))]
+    [(Prim op es) (Return (Prim op es))]
+    [else (error "Unrecognized expression (explicate-tail)" e)]))
+
+
+(define (explicate-assign e x cont)
+  (match e
+    [(Var y) (Seq (Assign (Var x) (Var y)) cont)]
+    [(Int n) (Seq (Assign (Var x) (Int n)) cont)]
+    [(Let y rhs body) (explicate-assign rhs y (explicate-assign body x cont))]
+    [(Prim op es) (Seq (Assign (Var x) (Prim op es)) cont)]
+    [else (error "Unrecognized expression (explicate-assign)" e)]))
+
 
 ;; explicate-control : R1 -> C0
 (define (explicate-control p)
-  (error "TODO: code goes here (explicate-control)"))
+  (match p
+    [(Program info body) (CProgram info (list (cons 'start (explicate-tail body))))]))
 
 ;; Define the compiler passes to be used by interp-tests and the grader
 ;; Note that your compiler file (the file that defines the passes)
 ;; must be named "compiler.rkt"
 (define compiler-passes
   `( 
-     ;; Uncomment the following passes as you finish them.
-     ;; ("uniquify" ,uniquify ,interp-Lvar ,type-check-Lvar)
-     ;; ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar ,type-check-Lvar)
-     ;; ("explicate control" ,explicate-control ,interp-Cvar ,type-check-Cvar)
-     ))
+    ;; Uncomment the following passes as you finish them.
+     ("uniquify" ,uniquify ,interp-Lvar ,type-check-Lvar)
+     ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar ,type-check-Lvar)
+     ("explicate control" ,explicate-control ,interp-Cvar ,type-check-Cvar)
+))
