1e2418fde3a99435e046bbcf0ceacf2665120b34
Kannav <kmkannavkmehta@gmail.com>
2023-02-01 21:25:13 +0530

    A2 code, tests left

--- compiler.rkt
+++ compiler.rkt
@@ -4,6 +4,7 @@
 (require "interp-Lint.rkt")
 (require "interp-Lvar.rkt")
 (require "interp-Cvar.rkt")
+(require "interp.rkt")
 (require "type-check-Lvar.rkt")
 (require "type-check-Cvar.rkt")
 (require "utilities.rkt")
@@ -85,13 +86,131 @@
   (match p
     [(Program info body) (CProgram info (list (cons 'start (explicate-tail body))))]))
 
+(define (select-instructions-atm atm)
+  (match atm
+    [(Int n) (Imm n)]
+    [(Var x) (Var x)]))
+
+(define (select-assign x e)
+  (define (get-op-instruction op)
+    (match op
+      [(Prim '+ (list e1 e2)) 'addq]
+      [(Prim '- (list e1 e2)) 'subq]))
+  (match e
+    [atm #:when (atm? atm) (list (Instr 'movq (list (select-instructions-atm atm) x)))]
+    [(Prim 'read '()) (list (Callq 'read_int 1) (Instr 'movq (list (Reg 'rax) x)))]
+    [(Prim '- (list e1)) #:when (equal? e1 x)
+                         (list
+                          (Instr 'negq (list (select-instructions-atm e1))))]
+    [(Prim '- (list e1)) (list
+                          (Instr 'movq (list (select-instructions-atm e1) x))
+                          (Instr 'negq (list x)))]
+    [(Prim op (list e1 e2)) #:when (equal? e1 x) (list
+                                                  (Instr (get-op-instruction e) (list (select-instructions-atm e2) x)))]
+    [(Prim op (list e1 e2)) #:when (equal? e2 x) (list
+                                                  (Instr (get-op-instruction e) (list (select-instructions-atm e1) x)))]
+    [(Prim op (list e1 e2)) (list
+                             (Instr 'movq            (list (select-instructions-atm e1) x))
+                             (Instr (get-op-instruction e) (list (select-instructions-atm e2) x)))]))
+    
+(define (select-instructions-stmt stmt)
+  (match stmt
+    [(Return e) (select-assign (Reg 'rax) e)]
+    [(Assign lhs rhs) (select-assign lhs rhs)]))
+
+(define (select-instructions-tail e)
+  (match e
+    [(Seq fst snd) (append (select-instructions-stmt fst) (select-instructions-tail snd))]
+    [(Return (Prim 'read '())) (list (Callq 'read_int 1) (Jmp 'conclusion))]
+    [(Return x) (append (select-instructions-stmt e) (list (Jmp 'conclusion)))]))
+
+;; select-instructions : C0 -> pseudo-X86
+(define (select-instructions p)
+  (match p
+    [(CProgram info (list (cons 'start t)))
+     (X86Program info (list (cons 'start (Block '() (select-instructions-tail t)))))]))
+
+(define (calculate-stack-frame env)
+  (cond
+    [(eq? (remainder (length env) 16) 0) (* 8 (length env))]
+    [else (* 8 (+ (length env) 1))]))
+
+(define (get-location x env)
+  (cond
+    [(eq? (length env) 0) 0]
+    [(eq? x (car (car env))) 1]
+    [else (+ 1 (get-location x (cdr env)))]))
+
+(define (assign-homes-imm i env)
+  (match i
+    [(Imm n) (Imm n)]
+    [(Reg r) (Reg r)]
+    [(Var x) (Deref 'rbp (* (- 8) (get-location x env)))]))
+    
+(define (assign-homes-instruction i env)
+  (match i
+    [(Instr name (list arg))       (Instr name (list (assign-homes-imm arg env)))]
+    [(Instr name (list arg1 arg2)) (Instr name (list (assign-homes-imm arg1 env)
+                                                     (assign-homes-imm arg2 env)))]
+    [else i]
+    ))
+
+;; assign-homes : pseudo-X86 -> pseudo-X86
+(define (assign-homes p)
+  (match p
+    [(X86Program info
+                 (list (cons 'start (Block blkinfo blkbody))))
+     (X86Program (dict-set info 'stack-space (calculate-stack-frame (dict-ref info 'locals-types)))
+                 (list (cons 'start (Block blkinfo (for/list ([instr blkbody])
+                                                     (assign-homes-instruction instr (dict-ref info 'locals-types)))))))]))
+
+(define (patch-single-instruction i)
+  (match i
+    [(Instr name (list arg1 arg2)) #:when (and (Deref? arg1) (Deref? arg2))
+                                   (list (Instr 'movq (list arg1 (Reg 'rax))) (Instr name (list (Reg 'rax) arg2)))]
+    [else (list i)]))
+
+;; assign-homes : pseudo-X86 -> X86
+(define (patch-instructions p)
+  (match p
+    [(X86Program info (list (cons 'start (Block blkinfo blkbody))))
+     (X86Program info (list (cons 'start (Block blkinfo (foldr
+                                                         (lambda (instr old) (append (patch-single-instruction instr) old)) '() blkbody)))))]))
+
+(define (generate-prelude info)
+  (list (cons 'main (Block '()
+                           (list
+                            (Instr 'pushq (list (Reg 'rbp)))
+                            (Instr 'movq (list (Reg 'rsp) (Reg 'rbp)))
+                            (Instr 'subq (list (Imm (dict-ref info 'stack-space)) (Reg 'rsp)))
+                            (Jmp 'start))))))
+
+
+(define (generate-conclusion info)
+  (list (cons 'conclusion (Block '()
+                           (list
+                            (Instr 'addq (list (Imm (dict-ref info 'stack-space)) (Reg 'rsp)))
+                            (Instr 'popq (list (Reg 'rbp)))
+                            (Retq))))))
+
+;; prelude-and-conclusion : X86 -> X86
+(define (prelude-and-conclusion p)
+  (match p
+    [(X86Program info block) (X86Program info (append (generate-prelude info) block (generate-conclusion info)))]))
+     
+
+
 ;; Define the compiler passes to be used by interp-tests and the grader
 ;; Note that your compiler file (the file that defines the passes)
 ;; must be named "compiler.rkt"
 (define compiler-passes
   `( 
     ;; Uncomment the following passes as you finish them.
-     ("uniquify" ,uniquify ,interp-Lvar ,type-check-Lvar)
-     ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar ,type-check-Lvar)
-     ("explicate control" ,explicate-control ,interp-Cvar ,type-check-Cvar)
-))
+    ("uniquify" ,uniquify ,interp-Lvar ,type-check-Lvar)
+    ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar ,type-check-Lvar)
+    ("explicate control" ,explicate-control ,interp-Cvar ,type-check-Cvar)
+    ("instruction selection", select-instructions ,interp-x86-0)
+    ("assign homes", assign-homes, interp-x86-0)
+    ("patch instructions", patch-instructions, interp-x86-0)
+    ("prelude and conclusion", prelude-and-conclusion, interp-x86-0)
+    ))
--- run-tests.rkt
+++ run-tests.rkt
@@ -25,9 +25,9 @@
         all-tests)))
 
 (debug-level 1)
-(interp-tests "var" #f compiler-passes interp-Lvar "var_test" (tests-for "var"))
+;; (interp-tests "var" #f compiler-passes interp-Lvar "var_test" (tests-for "var"))
 
 ;; Uncomment the following when all the passes are complete to
 ;; test the final x86 code.
-;; (compiler-tests "var" #f compiler-passes "var_test" (tests-for "var"))
+(compiler-tests "var" #f compiler-passes "var_test" (tests-for "var"))
 
